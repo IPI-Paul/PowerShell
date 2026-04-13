7389c4f65df46c758a22d7e0266f73415530c1ae
Paul I Ighofose <paul@ipi-international.co.uk>
2026-04-13 08:36:23 +0100

    Refactor Logger, add folder select
    PowerShell GUI and module to load CSV files into Excel whilst maintaining Long digit and leading zero numbers.

--- IPI Load and Preserve CSV in Excel/config/Config.psm1
+++ IPI Load and Preserve CSV in Excel/config/Config.psm1
@@ -6,10 +6,18 @@ function Get-LiteralPath {
 function Get-ScriptPaths {
     [PSCustomObject]@{
         IconHelper      = (Get-LiteralPath "..\cs\IPI Windows Icon Helper.cs")
+        WatermarkSvc    = (Get-LiteralPath "..\cs\IPI WPF Place Holder.cs")
         FilterFormula   = (Get-LiteralPath "..\md\IPI Excel Filtering Formula.md")
+        FolderSelect    = (Get-LiteralPath "..\psm\IPI Windows Folder Select.psm1")
         Engine          = (Get-LiteralPath "..\psm\IPI Load and Preserve CSV in Excel.psm1")
+        Logger          = (Get-LiteralPath "..\psm\IPI Rich Text Logger.psm1")
         IconPicker      = (Get-LiteralPath "..\psm\IPI Windows Icon Picker - Icons.psm1")
+        WebScraper      = (Get-LiteralPath "..\psm\IPI Web Scraper.psm1")
+        PlaceHolder     = (Get-LiteralPath "..\psm\IPI WPF Place Holder.psm1")
         MainWindow      = (Get-LiteralPath "..\xaml\IPI Load and Preserve CSV GUI.xaml")
         ZipWindow       = (Get-LiteralPath "..\xaml\IPI ZIP CSV Picker GUI.xaml")
+        Downloads       = (New-Object -ComObject Shell.Application).Namespace('shell:Downloads').Self.Path
     }
 }
+
+Export-ModuleMember -Function Get-ScriptPaths
